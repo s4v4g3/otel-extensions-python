b4fd76eead534f81af470e305e9dc02b318ca021
Joe Savage <joe.savage@silabs.com>
2023-01-03 17:17:59 -0600

    updated requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,1 @@
-from otel_extensions_pytest import TelemetryOptions
-
-pytest_plugins = ("pytest_asyncio", "otel_extensions_pytest")
-
-
-def pytest_addoption(parser, pluginmanager):
-    options = TelemetryOptions()
-    options.OTEL_SERVICE_NAME = "otel-extensions-python"
-    options.OTEL_SESSION_NAME = "unit tests pytest session"
-    options.OTEL_PROCESSOR_TYPE = "batch"
+pytest_plugins = "pytest_asyncio"
